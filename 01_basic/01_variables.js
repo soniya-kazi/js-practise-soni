a5384eb969247266edbf32d09a07a85244446975
soniya-kazi <150678962+soniya-kazi@users.noreply.github.com>
2023-11-13 13:30:39 +0000

    varaables details

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=12345
+let accountName="soniya"
+var accountEmail="soniyapriya@gmail.com"
+accountCity="bangalore"
+var accountState
+
+// var accountEmail="soniyapriya01@gmail.com"
+
+/*
+not prefered in scope or functional scope
+*/
+console.table([accountId,accountName,accountEmail,accountCity,accountState])
