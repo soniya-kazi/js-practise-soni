0b9b15d6f3a98a0089a043377df65d13a362a5b5
soniya-kazi <150678962+soniya-kazi@users.noreply.github.com>
2023-11-14 13:07:53 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all js code as newwer versiom
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+console.log("soniya")
+
+// ECMA script gives the standard of the js
+// MDN is the documentation for the js
+
+// ------Datatypes-------------
+
+let name="soniya"
+let age= 24
+let state;
+
+// ----details of datatypes-----------
+      // ----primitive of datatypes-----------
+
+// int => number
+// string => can be written in double or single quotes
+// big int => for large number eg trading
+// boolean => true/false
+// undefined => not defined
+// Null => object standalone object
+// symbols => for uniqueness used in react
+
+    // ---- NON primitive of datatypes-----------
+
+// object
+
+// function typeof
+console.log(typeof name)  // type is strings
+console.log(typeof age)   // type is number
+console.log(typeof state) // type is undefined
+console.log(typeof null)  // null type is object
+
+
+
